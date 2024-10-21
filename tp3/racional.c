d28e9e9d2e6cfd80d1fb784eea6030a06c538b09
CarlosASouto <carlossouto@ufpr.br>
2024-10-21 04:28:17 -0300

    tp3 pronto

--- tp3/racional.c
+++ tp3/racional.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Tipos Abstratos de Dados - TADs
  * Arquivo de implementação para TAD racional.
  * Feito em 20/09/2024 para a disciplina CI1001 - Programação 1.
@@ -6,7 +6,7 @@
  * Este arquivo deve conter as implementações das funções cujos protótipos
  * foram definidos em racional.h. Neste arquivo também podem ser definidas
  * funções auxiliares para facilitar a implementação daquelas funções.
-*/
+ */
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
@@ -17,25 +17,25 @@
 /* calcula o mdc pelo metodo de Euclides */
 long mdc (long a, long b)
 {
-  if (b==0)
-     return a;
+    if (b == 0)
+        return a;
 
-  return mdc (b, a % b);
+    return mdc (b, a % b);
 }
 
 /* Minimo Multiplo Comum entre a e b */
 /* mmc = (a * b) / mdc (a, b)        */
 long mmc (long a, long b)
 {
-  return ((a * b) / mdc (a, b));
+    return ((a * b) / mdc(a, b));
 }
 
 int valido_r (struct racional *r)
 {
-   if (r->den == 0 || r == NULL)
-     return 0;
-   
-   return 1;
+    if (r->den == 0 || r == NULL)
+        return 0;
+
+    return 1;
 }
 
 /* Simplifica o número racional indicado no parâmetro.
@@ -45,125 +45,128 @@ int valido_r (struct racional *r)
  * Se o denominador for negativo, o sinal deve migrar para o numerador. */
 int simplifica_r (struct racional *r)
 {
-  if (!valido_r (r) || r == NULL)
-     return 0;
+    if (!valido_r (r) || r == NULL)
+        return 0;
 
-  long divisor = mdc (r->num, r->den);
-  r->num = r->num / divisor;
-  r->den = r->den / divisor;
-  if (r->den < 0){
-     r->num = r->num * -1;
-     r->den = r->den * -1;
-  }
+    long divisor = mdc (r->num, r->den);
+    r->num = r->num / divisor;
+    r->den = r->den / divisor;
+    if (r->den < 0)
+    {
+        r->num = r->num * -1;
+        r->den = r->den * -1;
+    }
 
-  return 1;
+    return 1;
 }
 
 struct racional *cria_r (long numerador, long denominador)
 {
-  struct racional *r;
+    struct racional *r;
 
-  if (!(r = malloc (sizeof (struct racional))))
-  {
-     fprintf (stderr, "Alocacao de memoria falhou \n");
-     return NULL;
-  }
+    if (!(r = malloc (sizeof (struct racional))))
+    {
+        fprintf (stderr, "Alocacao de memoria falhou \n");
+        return NULL;
+    }
 
-  r->num = numerador;
-  r->den = denominador;
+    r->num = numerador;
+    r->den = denominador;
 
-  return r;
+    return r;
 }
 
 void destroi_r (struct racional *r)
 {
-  free (r);
+    free (r);
 }
 
 void imprime_r (struct racional *r)
 {
-    if (r == NULL){
+    if (r == NULL)
+    {
         printf ("NULL");
         return;
     }
-  simplifica_r (r);
+    simplifica_r (r);
 
-  if (!valido_r (r)){
-     printf ("NaN");
-     return;
-  }
-  if (r->num == 0 || r->den ==1){
-     printf ("%ld", r->num);
-     return;
-  }
-  if (r->num == r->den){
-     printf ("%d", 1);
-     return;
-  }
+    if (!valido_r (r))
+    {
+        printf ("NaN");
+        return;
+    }
+    if (r->num == 0 || r->den == 1)
+    {
+        printf ("%ld", r->num);
+        return;
+    }
+    if (r->num == r->den)
+    {
+        printf ("%d", 1);
+        return;
+    }
 
-  printf ("%ld/%ld", r->num, r->den);
+    printf ("%ld/%ld", r->num, r->den);
 }
 
 int compara_r (struct racional *r1, struct racional *r2)
 {
-  long den = mmc (r1->den, r2->den);
-  long n1 = den / r1->den * r1->num;
-  long n2 = den / r2->den * r2->num;
+    long den = mmc (r1->den, r2->den);
+    long n1 = den / r1->den * r1->num;
+    long n2 = den / r2->den * r2->num;
 
-  if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
-     return -2;
-  if (n1 < n2)
-     return -1;
-  if (n1 == n2)
-     return 0;
+    if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
+        return -2;
+    if (n1 < n2)
+        return -1;
+    if (n1 == n2)
+        return 0;
 
-  return 1;
+    return 1;
 }
 
 int soma_r (struct racional *r1, struct racional *r2, struct racional *r3)
 {
-  if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
-    return 0;
+    if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
+        return 0;
 
-  r3->den = mmc (r1->den, r2->den);
-  r3->num = (r3->den / r1->den * r1->num) + (r3->den / r2->den * r2->num);
+    r3->den = mmc (r1->den, r2->den);
+    r3->num = (r3->den / r1->den * r1->num) + (r3->den / r2->den * r2->num);
 
+    simplifica_r (r3);
 
-  simplifica_r (r3);
-
-  return 1;
+    return 1;
 }
 
 int subtrai_r (struct racional *r1, struct racional *r2, struct racional *r3)
 {
-  if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
-    return 0;
-  r3->den = mmc (r1->den, r2->den);
-  r3->num = (r3->den / r1->den * r1->num) - (r3->den / r2->den * r2->num);
-  simplifica_r (r3);
+    if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
+        return 0;
+    r3->den = mmc (r1->den, r2->den);
+    r3->num = (r3->den / r1->den * r1->num) - (r3->den / r2->den * r2->num);
+    simplifica_r (r3);
 
-  return 1;
+    return 1;
 }
 
 int multiplica_r (struct racional *r1, struct racional *r2, struct racional *r3)
 {
-  if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
-    return 0;
-  r3->den = r1->den * r2->den;
-  r3->num = r1->num * r2->num;
-  simplifica_r (r3);
+    if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
+        return 0;
+    r3->den = r1->den * r2->den;
+    r3->num = r1->num * r2->num;
+    simplifica_r (r3);
 
-  return 1;
+    return 1;
 }
 
 int divide_r (struct racional *r1, struct racional *r2, struct racional *r3)
 {
-  if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
-    return 0;
-  r3->den = r1->den * r2->num;
-  r3->num = r1->num * r2->den;
-  simplifica_r (r3);
+    if (!valido_r (r1) || !valido_r (r2) || r1 == NULL || r2 == NULL)
+        return 0;
+    r3->den = r1->den * r2->num;
+    r3->num = r1->num * r2->den;
+    simplifica_r (r3);
 
-  return 1;
+    return 1;
 }
-/* implemente as demais funções de racional.h aqui */
--- tp3/tp3.c
+++ tp3/tp3.c
@@ -1,8 +1,8 @@
-/* 
+/*
  * Tipos Abstratos de Dados - TADs
  * Arquivo do programa principal, que usa o TAD racional.
  * Feito em 24/09/2024 para a disciplina CI1001 - Programação 1.
-*/
+ */
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
@@ -11,15 +11,15 @@
 
 void imprimeVetor (struct racional **vet, int tam)
 {
-  printf ("VETOR = ");
-  for (int i = 0; i < tam; i++){
-    imprime_r (vet[i]);
-    if (i != tam - 1)
-        printf (" ");
-  }
-  printf ("\n");
+    printf ("VETOR = ");
+    for (int i = 0; i < tam; i++)
+    {
+        imprime_r (vet[i]);
+        if (i != tam - 1)
+            printf (" ");
+    }
+    printf ("\n");
 }
-
 
 void removeInvalidos (struct racional **vet, int *tam)
 {
@@ -27,13 +27,13 @@ void removeInvalidos (struct racional **vet, int *tam)
 
     for (i = 0; i < *tam; i++)
         if (valido_r (vet[i]))
-            vet[j++] = vet [i]; //Se valido, coloca o ponteiro na nova posicao
+            vet[j++] = vet[i]; // Se valido, coloca o ponteiro na nova posicao
         else
         {
-            destroi_r (vet[i]); //Se invalido libera a memoria
+            destroi_r (vet[i]); // Se invalido libera a memoria
             vet[i] = NULL;
         }
-    *tam = j; //Atualiza o tamanho do vetor
+    *tam = j; // Atualiza o tamanho do vetor
 }
 
 void trocaElementos (struct racional **vet, int a, int b)
@@ -43,78 +43,78 @@ void trocaElementos (struct racional **vet, int a, int b)
     vet[b] = temp;
 }
 
+/*Ordena o vetor por Bubble Sort*/
 void bubbleSort (struct racional **vet, int tam)
 {
-  if (tam < 1)
-     return;
-  
-  for (int i = 0; i < tam-1; i++)
-     if (compara_r (vet[i], vet[i + 1]) == 1) 
-        trocaElementos (vet, i, i + 1);
+    if (tam < 1)
+        return;
 
-  bubbleSort (vet, tam-1);
+    for (int i = 0; i < tam - 1; i++)
+        if (compara_r (vet[i], vet[i + 1]) == 1)
+            trocaElementos (vet, i, i + 1);
+
+    bubbleSort (vet, tam - 1);
 }
 /* programa principal */
 int main ()
 {
-  int n = 0;
-  long num, den;
-  struct racional **vet;
-  struct racional *soma = cria_r (0, 1);
-  struct racional *aux = cria_r (0, 1);
+    int n = 0;
+    long num, den;
+    struct racional **vet;
+    struct racional *soma = cria_r (0, 1);
+    struct racional *aux = cria_r (0, 1);
 
-  while (n <= 0 || n >= 100)
-     scanf ("%d", &n);
-  
-  if (!(vet = malloc (sizeof(struct racional *) * n)))
-  {
-     fprintf (stderr, "Alocacao de memoria falhou\n");
-     exit(1);
-  }
+    while (n <= 0 || n >= 100)
+        scanf ("%d", &n);
 
-  for (int i = 0; i < n; i++)
-  {
-    scanf ("%ld %ld", &num, &den);
-    vet[i] = cria_r (num, den);
-  }
+    if (!(vet = malloc (sizeof (struct racional *) * n)))
+    {
+        fprintf (stderr, "Alocacao de memoria falhou\n");
+        exit (1);
+    }
 
-  imprimeVetor (vet, n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf ("%ld %ld", &num, &den);
+        vet[i] = cria_r (num, den);
+    }
 
-  removeInvalidos (vet, &n);
+    imprimeVetor (vet, n);
 
-  imprimeVetor (vet, n);
+    removeInvalidos (vet, &n);
 
-  bubbleSort (vet, n);
+    imprimeVetor (vet, n);
 
-  imprimeVetor (vet, n);
+    bubbleSort (vet, n);
 
-  for (int i = 0; i < n; i++)
-  {  
-    soma_r (soma, vet[i], aux);
-    *soma = *aux;
-  }
+    imprimeVetor (vet, n);
 
-  destroi_r (aux);
-  aux = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        soma_r (soma, vet[i], aux);
+        *soma = *aux;
+    }
 
-  printf ("SOMA = ");
-  imprime_r (soma);
-  printf ("\n");
+    destroi_r (aux);
+    aux = NULL;
 
-  for (int i = 0; i < n; i++)
-  {
-    destroi_r (vet [i]);
-    vet[i] = NULL;
-  }
+    printf ("SOMA = ");
+    imprime_r (soma);
+    printf ("\n");
 
-  imprimeVetor (vet, n);
+    for (int i = 0; i < n; i++)
+    {
+        destroi_r (vet[i]);
+        vet[i] = NULL;
+    }
 
-  free (vet);
-  vet = NULL;
+    imprimeVetor (vet, n);
 
-  destroi_r (soma);
-  soma = NULL;
+    free (vet);
+    vet = NULL;
 
-  return 0;
+    destroi_r (soma);
+    soma = NULL;
+
+    return 0;
 }
-
