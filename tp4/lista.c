3634cf703762d760d9d3f0ac4849eb89c65ff56e
CarlosASouto <151091222+CarlosASouto@users.noreply.github.com>
2024-10-29 15:56:11 -0300

    Tp4 inicio falta implementar lista.c

---
+++ tp4/lista.c
@@ -0,0 +1,47 @@
+// TAD lista de números inteiros
+// Carlos Maziero - DINF/UFPR, Out 2024
+//
+// Implementação do TAD - a completar
+//
+// Implementação com lista encadeada dupla não-circular
+
+
+struct lista_t *lista_cria ()
+{
+    
+}
+
+struct lista_t *lista_destroi (struct lista_t *lst)
+{
+
+}
+
+int lista_insere (struct lista_t *lst, int item, int pos)
+{
+
+}
+
+int lista_retira (struct lista_t *lst, int *item, int pos)
+{
+
+}
+
+int lista_consulta (struct lista_t *lst, int *item, int pos)
+{
+
+}
+
+int lista_procura (struct lista_t *lst, int valor)
+{
+
+}
+
+int lista_tamanho (struct lista_t *lst)
+{
+
+}
+
+void lista_imprime (struct lista_t *lst)
+{
+    
+}
